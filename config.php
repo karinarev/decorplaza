a808b85aff66ebb0cbacd745ae94cbbffb0ba060
karinarev <karina.r.161@yandex.ru>
2016-09-09 15:53:56 +0300

    config

--- config.php
+++ config.php
@@ -1,28 +1,28 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://plaza/');
+define('HTTP_SERVER', 'http://decor-plaza.dev-gns.com/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://plaza/');
+define('HTTPS_SERVER', 'http://decor-plaza.dev-gns.com/');
 
 // DIR
-define('DIR_APPLICATION', 'C:/OpenServer/domains/plaza/catalog/');
-define('DIR_SYSTEM', 'C:/OpenServer/domains/plaza/system/');
-define('DIR_DATABASE', 'C:/OpenServer/domains/plaza/system/database/');
-define('DIR_LANGUAGE', 'C:/OpenServer/domains/plaza/catalog/language/');
-define('DIR_TEMPLATE', 'C:/OpenServer/domains/plaza/catalog/view/theme/');
-define('DIR_CONFIG', 'C:/OpenServer/domains/plaza/system/config/');
-define('DIR_IMAGE', 'C:/OpenServer/domains/plaza/image/');
-define('DIR_CACHE', 'C:/OpenServer/domains/plaza/system/cache/');
-define('DIR_DOWNLOAD', 'C:/OpenServer/domains/plaza/download/');
-define('DIR_LOGS', 'C:/OpenServer/domains/plaza/system/logs/');
+define('DIR_APPLICATION', '/var/www/dev_gns/data/www/decor-plaza.dev-gns.com/catalog/');
+define('DIR_SYSTEM', '/var/www/dev_gns/data/www/decor-plaza.dev-gns.com/system/');
+define('DIR_DATABASE', '/var/www/dev_gns/data/www/decor-plaza.dev-gns.com/system/database/');
+define('DIR_LANGUAGE', '/var/www/dev_gns/data/www/decor-plaza.dev-gns.com/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/dev_gns/data/www/decor-plaza.dev-gns.com/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/dev_gns/data/www/decor-plaza.dev-gns.com/system/config/');
+define('DIR_IMAGE', '/var/www/dev_gns/data/www/decor-plaza.dev-gns.com/image/');
+define('DIR_CACHE', '/var/www/dev_gns/data/www/decor-plaza.dev-gns.com/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/dev_gns/data/www/decor-plaza.dev-gns.com/download/');
+define('DIR_LOGS', '/var/www/dev_gns/data/www/decor-plaza.dev-gns.com/system/logs/');
 
 // DB
 define('DB_DRIVER', 'mysql');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'opencartdb');
-define('DB_PASSWORD', 'Ex53WEDuyD98WzoR');
-define('DB_DATABASE', 'decor-plaza');
+define('DB_USERNAME', 'decorplaza');
+define('DB_PASSWORD', '12345qwert');
+define('DB_DATABASE', 'decorplaza');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
 
