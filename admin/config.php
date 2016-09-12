c01a7d13c94906af902d61c78f306c62fca5b24e
karinarev <karina.r.161@yandex.ru>
2016-09-12 13:43:18 +0300

    header, footer, contacts, info, product (reverted from commit 159bcc45ad9d21e8bbad5de0b38fffc459a68a3b) (reverted from commit faf99f6e8b99f995e6b9b509baa81d5df93e357b)

--- admin/config.php
+++ admin/config.php
@@ -1,30 +1,30 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://decor-plaza.dev-gns.com/admin/');
-define('HTTP_CATALOG', 'http://decor-plaza.dev-gns.com/');
+define('HTTP_SERVER', 'http://plaza/admin/');
+define('HTTP_CATALOG', 'http://plaza/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://decor-plaza.dev-gns.com/admin/');
-define('HTTPS_CATALOG', 'http://decor-plaza.dev-gns.com/');
+define('HTTPS_SERVER', 'http://plaza/admin/');
+define('HTTPS_CATALOG', 'http://plaza/');
 
 // DIR
-define('DIR_APPLICATION', '/var/www/dev_gns/data/www/decor-plaza.dev-gns.com/admin/');
-define('DIR_SYSTEM', '/var/www/dev_gns/data/www/decor-plaza.dev-gns.com/system/');
-define('DIR_DATABASE', '/var/www/dev_gns/data/www/decor-plaza.dev-gns.com/system/database/');
-define('DIR_LANGUAGE', '/var/www/dev_gns/data/www/decor-plaza.dev-gns.com/admin/language/');
-define('DIR_TEMPLATE', '/var/www/dev_gns/data/www/decor-plaza.dev-gns.com/admin/view/template/');
-define('DIR_CONFIG', '/var/www/dev_gns/data/www/decor-plaza.dev-gns.com/system/config/');
-define('DIR_IMAGE', '/var/www/dev_gns/data/www/decor-plaza.dev-gns.com/image/');
-define('DIR_CACHE', '/var/www/dev_gns/data/www/decor-plaza.dev-gns.com/system/cache/');
-define('DIR_DOWNLOAD', '/var/www/dev_gns/data/www/decor-plaza.dev-gns.com/download/');
-define('DIR_LOGS', '/var/www/dev_gns/data/www/decor-plaza.dev-gns.com/system/logs/');
-define('DIR_CATALOG', '/var/www/dev_gns/data/www/decor-plaza.dev-gns.com/catalog/');
+define('DIR_APPLICATION', 'C:/OpenServer/domains/plaza/admin/');
+define('DIR_SYSTEM', 'C:/OpenServer/domains/plaza/system/');
+define('DIR_DATABASE', 'C:/OpenServer/domains/plaza/system/database/');
+define('DIR_LANGUAGE', 'C:/OpenServer/domains/plaza/admin/language/');
+define('DIR_TEMPLATE', 'C:/OpenServer/domains/plaza/admin/view/template/');
+define('DIR_CONFIG', 'C:/OpenServer/domains/plaza/system/config/');
+define('DIR_IMAGE', 'C:/OpenServer/domains/plaza/image/');
+define('DIR_CACHE', 'C:/OpenServer/domains/plaza/system/cache/');
+define('DIR_DOWNLOAD', 'C:/OpenServer/domains/plaza/download/');
+define('DIR_LOGS', 'C:/OpenServer/domains/plaza/system/logs/');
+define('DIR_CATALOG', 'C:/OpenServer/domains/plaza/catalog/');
 /// DB
 define('DB_DRIVER', 'mysql');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'decorplaza');
-define('DB_PASSWORD', '12345qwert');
-define('DB_DATABASE', 'decorplaza');
+define('DB_USERNAME', 'opencartdb');
+define('DB_PASSWORD', 'Ex53WEDuyD98WzoR');
+define('DB_DATABASE', 'decor-plaza');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
 
--- vqmod/vqcache/vq2-catalog_model_catalog_product.php
+++ vqmod/vqcache/vq2-catalog_model_catalog_product.php
@@ -270,8 +270,7 @@ class ModelCatalogProduct extends Model {
 			'p.price',
 			'rating',
 			'p.sort_order',
-			'p.date_added',
-			'p.sku'
+			'p.date_added'
 		);	
 		
 		if (isset($data['sort']) && in_array($data['sort'], $sort_data)) {
@@ -433,13 +432,7 @@ class ModelCatalogProduct extends Model {
 			}
 			
 			if (empty($productID)){
-					/*$query = $this->db->query("SELECT p.product_id FROM " . DB_PREFIX . "product p WHERE p.status = '1' AND p.bestSaller = '1' AND p.date_available <= '" . $this->NOW . "' ORDER BY p.product_id DESC");*/
-					$offset_result = $this->db->query("SELECT FLOOR(RAND() * COUNT(*)) AS `offset` FROM " . DB_PREFIX . "product");
-					$offset = $offset_result->row['offset'];
-					$query = $this->db->query("
-					 SELECT * FROM " . DB_PREFIX . "product p 
-					 WHERE p.quantity > 0
-					 LIMIT $offset, 1 " );
+					$query = $this->db->query("SELECT p.product_id FROM " . DB_PREFIX . "product p WHERE p.status = '1' AND p.bestSaller = '1' AND p.date_available <= '" . $this->NOW . "' ORDER BY p.product_id DESC");
 
 					foreach ($query->rows as $result) {
 						$arProducts[] = $result['product_id'];
@@ -1357,6 +1350,7 @@ public function getAjaxcartProducts($data = array()) {
 				return $product_data;
 		}
 }
+	
 	public function getTotalProductSpecials() {
 		if ($this->customer->isLogged()) {
 			$customer_group_id = $this->customer->getCustomerGroupId();
@@ -1379,8 +1373,5 @@ public function getAjaxcartProducts($data = array()) {
 		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "city c WHERE c.name='" . $city . "'");
 		return $query->row;
 	}
-
-	
-
 }
 ?>
