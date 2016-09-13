b0ad501ebb37ae1fb1578f6316f4a181570340f9
karinarev <karina.r.161@yandex.ru>
2016-09-13 12:06:37 +0300

    + config

--- config.php
+++ config.php
@@ -18,12 +18,11 @@ define('DIR_DOWNLOAD', '/var/www/dev_gns/data/www/decor-plaza.dev-gns.com/downlo
 define('DIR_LOGS', '/var/www/dev_gns/data/www/decor-plaza.dev-gns.com/system/logs/');
 
 // DB
-define('DB_DRIVER', 'mysql');
+define('DB_DRIVER', 'mysqliz');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'decorplaza');
 define('DB_PASSWORD', '12345qwert');
 define('DB_DATABASE', 'decorplaza');
-define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
 
 ?>
