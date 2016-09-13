7228580623e626e7e817297326a9d5c5ba8c17c8
karinarev <karina.r.161@yandex.ru>
2016-09-13 12:07:57 +0300

    config db

--- admin/config.php
+++ admin/config.php
@@ -20,7 +20,7 @@ define('DIR_DOWNLOAD', '/var/www/dev_gns/data/www/decor-plaza.dev-gns.com/downlo
 define('DIR_LOGS', '/var/www/dev_gns/data/www/decor-plaza.dev-gns.com/system/logs/');
 define('DIR_CATALOG', '/var/www/dev_gns/data/www/decor-plaza.dev-gns.com/catalog/');
 /// DB
-define('DB_DRIVER', 'mysqliz');
+define('DB_DRIVER', 'mysqli');
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'decorplaza');
 define('DB_PASSWORD', '12345qwert');
